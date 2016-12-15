1bd6a464235768f59760abe760c3cfc4007c6a9c
Haider Rehman <haiderktk@users.noreply.github.com>
2016-12-15 01:51:35 +0100

    Add files via upload

---
+++ project/StandardConstants.java
@@ -0,0 +1,29 @@
+package butp;
+
+/**
+ *
+ * @author Haider Rehman, Varaynya Chivukula
+ */
+public class StandardConstants {
+    
+        /**
+         * opcode  operation
+         *   1     Read request (RRQ)
+         *   2     Write request (WRQ)
+         *   3     Data (DATA)
+         *   4     Acknowledgment (ACK)
+         *   5     Error (ERROR)
+         * 
+         */
+    
+    
+        static final int RRQ    = 1;
+	static final int WRQ    = 2;
+	static final int DATA   = 3;
+	static final int ACK    = 4;
+	static final int NAK    = 5;
+        
+        static String temstr ="";
+
+}
+
